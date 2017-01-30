d7246a79fd4b520e44b67a60e481db93c3438365
“Aaron <barfootaaron@yahoo.com>
2017-01-30 15:32:28 -0600

    read messages working

--- javascripts/iife3.js
+++ javascripts/iife3.js
@@ -12,5 +12,14 @@ var Chatty = (function(oldChatty){
 		var messageToDelete = document.getElementById("message--" + idIndex);
 		messageToDelete.parentNode.removeChild(messageToDelete);
 	}
+
+	oldChatty.readMessage = function(event) {
+		var idIndex = ((event.target.id).substring(6, 7)).toString();
+		var messageToRead = document.getElementById("message--" + idIndex);
+		var x = messageToRead.childNodes[1].childNodes[0].nodeValue;
+		console.log(event.target.id);
+		responsiveVoice.speak(x, "UK English Male", {rate: 0.8});
+	} 
+
 	return oldChatty;
 })(Chatty || {});
